0b064b281f0ab028eb97cbf0484ad202d3d53cc2
Bruno Silva <s.bruno.12@hotmail.com>
2023-12-18 07:48:01 -0300

    first commit

---
+++ src/Streams/Main.java
@@ -0,0 +1,40 @@
+package Streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Pessoa> listaPessoas = lerPessoasDoConsole();
+        
+        List<Pessoa> mulheres = listaPessoas.stream()
+                .filter(pessoa -> "Feminino".equalsIgnoreCase(pessoa.sexo))
+                .collect(Collectors.toList());
+
+        System.out.println("Lista de mulheres:");
+        mulheres.forEach(pessoa -> System.out.println("Nome: " + pessoa.nome + ", Sexo: " + pessoa.sexo));
+    }
+
+    private static List<Pessoa> lerPessoasDoConsole() {
+        Scanner scanner = new Scanner(System.in);
+        List<Pessoa> listaPessoas = new ArrayList<>();
+
+        System.out.println("Digite o número de pessoas:");
+        int numPessoas = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numPessoas; i++) {
+            System.out.println("Digite o nome da pessoa " + (i + 1) + ":");
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite o sexo da pessoa " + (i + 1) + " (Masculino/Feminino):");
+            String sexo = scanner.nextLine();
+
+            listaPessoas.add(new Pessoa(nome, sexo));
+        }
+
+        return listaPessoas;
+    }
+}
